c1985c04a6538faed83a84f3cdcb7bccce90967a
임제훈 <Hmax3J@gmail.com>
2022-08-28 01:49:53 +0900

    2022.08.27

---
+++ review/weekreview/1.core/3.jquery.js
@@ -0,0 +1,53 @@
+$('#myId')
+
+$('.myClass')
+
+$('input[name="surname"]')
+
+$('#contents ul.people li')
+
+$('a.external:first')
+
+$('li:odd')
+
+$('div:visible')
+
+$('div:gt(1)')
+
+$('div:animated')
+
+$('a[rel$="thinger"]')
+
+$('div.foo').has('p')
+
+$('ul li').filter('.current')
+
+$('ul li').not('.current')
+
+$('ul li').eq(1)
+
+$('form :button')
+
+$('form :radio')
+$('form :checkbox')
+
+$('form :checked')
+
+$('form :selected')
+
+$('form :enabled')
+$('form :diabled')
+
+$('form :file')
+
+$('form :input')
+
+$('form :text')
+
+$('form :password')
+
+$('form input[name="gender"]:radio')
+
+$('form: reset')
+
+$('form: submit')
